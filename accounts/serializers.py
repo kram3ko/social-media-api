ae39afe3929418b5d31752811598b64bbf698d89
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-05-14 08:44:47 +0300

    Introduce custom user model and implement JWT authentication
    Created a custom User model with email as the primary identifier. Added endpoints for registration and JWT-based authentication, including token issuance, refresh, and verification. Configured REST framework and integrated `djangorestframework-simplejwt` for token authentication.

---
+++ accounts/serializers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.utils.translation import gettext as _
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        label=_("Password"),
+        style={"input_type": "password"},
+        validators=[validate_password],
+    )
+    password2 = serializers.CharField(
+        style={"input_type": "password"},
+        min_length=8,
+        label=_("Confirm Password"),
+        write_only=True,
+    )
+
+    class Meta:
+        model = User
+        fields = ["email", "username", "password", "password2"]
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": _("Passwords don't match.")})
+        attrs.pop("password2", None)
+        return attrs
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, set the password correctly and return it"""
+        if password := validated_data.pop("password", None):
+            instance.set_password(password)
+        return super().update(instance, validated_data)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from accounts.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
